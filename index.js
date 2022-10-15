acf8732ee28b36fd11d20f48189ff5094b62a2b7
newt <axelskam.123@outlook.es>
2022-10-15 00:05:51 -0500

    update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("node:fs");
 const { Collection, Client, GatewayIntentBits } = require("discord.js");
 const { Player } = require("discord-player");
-const { token } = require("./config.json");
+// const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({
